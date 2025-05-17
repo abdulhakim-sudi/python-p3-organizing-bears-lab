bdeefebab353ba2bb2d087a128e152f0199be7f7
Abdulhakim Sudi <abdulhakimsudi27@gmail.com>
2025-05-17 22:16:00 +0300

    Insert 8 bears with all required fields including one unnamed bear

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 10, 'M', 'brown', 'calm', 1),
+('Rowdy', 5, 'M', 'black', 'aggressive', 1),
+('Tabitha', 7, 'F', 'golden', 'friendly', 1),
+('Sergeant Brown', 12, 'M', 'brown', 'stern', 1),
+('Melissa', 6, 'F', 'white', 'playful', 1),
+('Grinch', 8, 'M', 'green', 'grumpy', 0),
+('Wendy', 4, 'F', 'gray', 'cheerful', 1),
+(NULL, 3, 'M', 'brown', 'shy', 0); -- unnamed bear
